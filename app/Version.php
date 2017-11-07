2806df1e7cacf2309425daa1299b77325326a46f
samuthojo <samuthojo@gmail.com>
2017-11-07 15:17:50 +0300

    A little refactoring
    modified:   app/Price.php
    
    modified:   app/Version.php

--- app/Version.php
+++ app/Version.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Version extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id', ];
 }
