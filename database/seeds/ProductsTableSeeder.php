3a91ceee773f85be4ae7b79b8ff1439f49857035
samuthojo <samuthojo@gmail.com>
2017-11-29 15:31:49 +0300

    modified:   database/seeds/CategoriesTableSeeder.php

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,16 +11,16 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-      $faker = Faker\Factory::create();
-      for($i = 1; $i < 7; $i++) {
-        \App\Product::create([
-          'category_id' => $i,
-          'name' => $faker->word(1),
-          'code' => 'ABA',
-          'cc' => $faker->numberBetween(1,5),
-          'image' => '1.jpg',
-          'description' => $faker->paragraph(3, true),
-        ]);
-      }
+      // $faker = Faker\Factory::create();
+      // for($i = 1; $i < 7; $i++) {
+      //   \App\Product::create([
+      //     'category_id' => $i,
+      //     'name' => $faker->word(1),
+      //     'code' => 'ABA',
+      //     'cc' => $faker->numberBetween(1,5),
+      //     'image' => '1.jpg',
+      //     'description' => $faker->paragraph(3, true),
+      //   ]);
+      // }
     }
 }
