76b9dbfabbc3c8d21168b955071f1a70cccd5242
samuthojo <samuthojo@gmail.com>
2018-05-21 09:23:40 +0300

    modified:   app/Http/Requests/CreateUser.php
    new file:   database/migrations/2018_05_21_061738_add_unique_to_business_id.php

--- app/Http/Requests/CreateUser.php
+++ app/Http/Requests/CreateUser.php
@@ -31,7 +31,7 @@ class CreateUser extends FormRequest
             'subscription_start_date' => 'required',
             'subscription_end_date' => 'required',
             'total_cc' => 'required',
-            'business_id' => 'nullable|unique:users',
+            'business_id' => 'required|unique:users',
             'current_level' => 'required',
             'country' => 'required',
             'device_id' => 'required',
