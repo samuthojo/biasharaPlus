9528e84481af1b6388ee7f7eb16cfd5e5daa56ef
samuthojo <samuthojo@gmail.com>
2018-06-11 22:39:01 +0300

    modified:   public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1381,8 +1381,9 @@ module.exports = __webpack_require__(54);
 
 /***/ }),
 /* 13 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
 
 /**
  * First we will load all of this project's JavaScript dependencies which
